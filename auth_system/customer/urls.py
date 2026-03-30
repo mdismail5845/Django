b9515a418c64a5ad24e17e9d4dbd76d3364c6362
ismail <mdismail5845@gmail.com>
2026-03-30 16:07:16 +0600

    Re-arrange files

--- auth_system/customer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from customer import views
-
-urlpatterns = [
-    path('customer_dashboard',views.customer_dashboard, name='customer_dashboard')
-]
--- auth_system/seller/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from seller import views
-
-urlpatterns = [
-    path('seller_dashboard', views.seller_dashboard, name='seller_dashboard')
-]
